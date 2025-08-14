e1891ffd1c8a111764f61686db19c74439dcb440
Vempra-turismo <gerencia.vempra@gmail.com>
2025-08-14 16:26:32 -0300

    feat: Configurar adaptador de Vercel para SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src')
+      }
+    }
+  },
   integrations: [
     tailwind(),
     icon({
